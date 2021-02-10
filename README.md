0b9a37ca6394893c09f2031ecbbc0c2d3d4272a5
Kiwi <hallerlucas@outlook.com>
2021-02-11 00:22:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image-localizer
+ Locate the place where a photo was taken
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+__author__ = "Kwalix - hallerlucas@outlook.com"
+__version__ = "0.1.0"
+
