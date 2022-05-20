c4b3f2fbeac7371aa8819c017906103fc59899ba
kwalixx <kwalixx@protonmail.com>
2022-05-20 10:31:50 +0200

    Some Update
    Changing setup.py version; Changing main script from main.py -> ImageLocalizer.py; updating requirements.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from cx_Freeze import setup, Executable
 import sys
+from ImageLocalizer import __version__
 
-build_exe_options = {"packages": ["sys", "webbrowser", "GPSPhoto", "PySide2"], 'include_files': ["img/"]}
+build_exe_options = {"packages": [
+    "sys", "webbrowser", "GPSPhoto", "PySide2"], 'include_files': ["img/"]}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(name="Image localizer",
-      version="1.0.0",
-      author="Kwalix",
+      version=__version__,
+      author="Kwalixx",
       description="Gui tool to extract GPS datas from image",
       options={"build_exe": build_exe_options},
-      executables=[Executable("main.py", base=base, icon="img/imageLocalizer-icons.ico")])
+      executables=[Executable("ImageLocalizer.py", base=base, icon="img/imageLocalizer-icons.ico")])
